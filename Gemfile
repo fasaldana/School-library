e179ae9623b56ab9f87957392fafb058348d898c
fasaldana <fernando_s_98@hotmail.com>
2022-06-13 15:31:40 -0500

    Added source files

--- Gemfile
+++ Gemfile
@@ -4,4 +4,6 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
+gem 'rubocop-lsp', '~> 0.1.5'
+
 # gem "rails"
---
+++ src/person.rb
@@ -0,0 +1,20 @@
+class Person
+    attr_reader :id
+    attr_accessor :name, :age
+    def initialize(name = 'unknown', age, parent_permission = true)
+        @id = id
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+
+    def can_use_services?
+        is_of_age? && @parent_permission
+    end
+
+    private
+
+    def is_of_age?
+        @age >= 18
+    end
+end
