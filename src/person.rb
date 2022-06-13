2ded30b22d5aaf1e4af5b48ee16edf6e6c8fb146
fasaldana <fernando_s_98@hotmail.com>
2022-06-13 17:16:04 -0500

    fix minor issues

--- src/person.rb
+++ src/person.rb
@@ -3,14 +3,14 @@ class Person
   attr_accessor :name, :age
 
   def initialize(age, name = 'unknown', parent_permission: true)
-    @id = id
+    @id = rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   def can_use_services?
-    of_age? && @parent_permission
+    of_age? || @parent_permission
   end
 
   private
