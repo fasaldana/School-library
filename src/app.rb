a1980887262ce533a3d11d2d14145ea48bc94b60
fasaldana <fernando_s_98@hotmail.com>
2022-06-23 10:30:17 -0500

    generate other files

--- src/app.rb
+++ src/app.rb
@@ -53,18 +53,48 @@ class App
 
   def load_data
     file_handle = FileHandle.new
-    if(file_handle.read_file)
-      @books = file_handle.read_file
+    if(file_handle.read_book)
+      file_handle.read_book.each do |book|
+        @books << Book.new(book['title'], book['author'])
+      end
+    end
+    if(file_handle.read_person)
+      file_handle.read_person.each do |person|
+        if person['type'] == 'student'
+          @people << Student.new(person['age'], person['name'], parent_permission: person['parent_permission'])
+        else
+          @people << Teacher.new(person['age'], person['name'], person['specialization'])
+        end
+      end
+    end
+    if(file_handle.read_rental)
+      file_handle.read_rental.each do |rental|
+        @rentals << Rental.new(rental['date'],  rental['book'], rental['person'])
+      end
     end
   end
 
   def save_info
-    books = []
+    book = []
+    person = []
+    rental = []
     file_handle = FileHandle.new
     @books.each_with_index do |book, index|
       obj = {id: index, title: book.title, author: book.author}
       books.push(obj)
     end
-    file_handle.create_file(books)
+    @people.each_with_index do |person1, index|
+      if(person1.class == Student)
+        person.append({id: index + 1, type: 'student', age: person1.age, name: person1.name, parent_permission: person1.parent_permission})
+      else
+        person.append({id: index + 1, type: 'teacher', age: person1.age, name: person1.name, specialization: person1.specialization})
+      end
+    end
+    @rentals.each_with_index do |rental1, index|
+      rental.append({id: index + 1, date: rental1.date, book: rental1.book.title, person: rental1.person.name})
+    end
+    file_handle.create_book(book)
+    file_handle.create_person(person)
+    file_handle.create_rental(rental)
   end
 end
--- src/file_handle.rb
+++ src/file_handle.rb
@@ -1,16 +1,48 @@
 require 'json'
 
 class FileHandle
-  def create_file (books)
-    p books
+  def create_book (book)
     File.open("book.json", "w") do |f|
       f.write(JSON.pretty_generate(books))
     end
   end
 
-  def read_file
-    file = File.open('book.json', 'r')
-    data = JSON.load file
-    file.close
+  def read_book
+    if(File.exist?("book.json"))
+      file = File.open "./book.json"
+      data = JSON.load file
+      file.close
+    end
+    return data
+  end
+
+  def create_person (person)
+    File.open("person.json", "w") do |f|
+        f.write(person.to_json)
+    end
+  end
+
+  def read_person
+    if(File.exist?("person.json"))
+      file = File.open "./person.json"
+      data = JSON.load file
+      file.close
+    end
+    return data
+  end
+
+  def create_rental (rental)
+    File.open("rental.json", "w") do |f|
+        f.write(rental.to_json)
+    end
+  end
+
+  def read_rental
+    if(File.exist?("rental.json"))
+      file = File.open "./rental.json"
+      data = JSON.load file
+      file.close
+    end
+    return data
   end
 end
